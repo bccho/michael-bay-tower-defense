4fd5ba70e864cfa904706914c0cfda23cb10a246
Byung Cheol Cho <bccho@princeton.edu>
2018-05-10 16:03:32 -0400

    Enemy update() with delta_t and SimpleEnemy for quick and dirty testing

--- js/enemy.js
+++ js/enemy.js
@@ -22,9 +22,25 @@ function Enemy(kwargs) {
 
 Enemy.prototype = new GameObject();
 
-Enemy.prototype.update = function() {
-    this._position.add(this._velocity);
+Enemy.prototype.update = function(delta_t) {
+    this._position.add(this._velocity.clone().multiplyScalar(delta_t));
 
     // Call base method
     GameObject.prototype.update.call(this);
 };
+
+
+// Simple enemy for quick and dirty testing. Inherits from Enemy
+function SimpleEnemy(kwargs) {
+    var radius = 10.0;
+    var phong = new THREE.MeshPhongMaterial( {color: 0xFF0000} );
+    var body = new THREE.Mesh(new THREE.SphereGeometry(radius, 32, 32), phong);
+    body.position.set(0.0, radius, 0.0);
+
+    kwargs = setDefault(kwargs, "meshes", [body]);
+    Enemy.call(this, kwargs);
+
+    return this;
+}
+
+SimpleEnemy.prototype = new Enemy();
