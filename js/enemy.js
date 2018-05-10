c1e4b35cd0f3874373c953466b1ac61efddc2e70
Byung Cheol Cho <bccho@princeton.edu>
2018-05-10 15:39:02 -0400

    Simple enemy movement

--- js/enemy.js
+++ js/enemy.js
@@ -1,11 +1,15 @@
+// Enemy inherits from GameObject
 function Enemy(kwargs) {
     kwargs = kwargs || {};
+
+    // Initialize member variables
+    this._velocity = new THREE.Vector3();
 
     // Parse options
     for (var option in kwargs) {
         var value = kwargs[option];
-        if (option === "BLAH") {
-            obj._BLAH = value;
+        if (option === "velocity") {
+            this._velocity = value;
         } else {
             console.log("Unknown option " + option + "! Make sure to register it!");
         }
@@ -19,3 +23,9 @@ function Enemy(kwargs) {
 
 Enemy.prototype = new GameObject();
 
+Enemy.prototype.update = function() {
+    this._position.add(this._velocity);
+
+    // Call base method
+    GameObject.prototype.update.call(this);
+};
