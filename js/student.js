991d5bcf19bd8f351ce323e7cf7685629b1a8cb4
Byung Cheol Cho <bccho@princeton.edu>
2018-05-11 18:44:07 -0400

    Updated HTML text and student info

--- js/student.js
+++ js/student.js
@@ -1,8 +1,8 @@
 var Student = {
     // please fill in your name and NetID
     // your NetID is the part of your email before @princeton.edu
-    "name"  : "Byung-Cheol Cho",
-    "netID" : "bccho",
+    "name"  : "Byung-Cheol Cho, Kraig McFadden, Grant Schultheis",
+    "netID" : "bccho, kraigm, wgs2",
 };
 
 Student.updateHTML = function( ) {
